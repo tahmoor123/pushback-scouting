b5c24d9f24e6821874632838c73b69a703b9c97b
Tmoor <tahmoor123@gmail.com>
2026-02-24 17:33:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ database.ref("matches").on("value", snapshot=>{
       console.log("Match entry:", m);
     });
   }
-  let statsDiv = document.getElementById("stats");
+  let statsDiv = document.getElementById("match");
 
   if(!statsDiv) return;
 
@@ -291,6 +291,7 @@ function getScoutName(){
 // Initialize
 loadTeams();
 showPage("pit");
+
 
 
 
